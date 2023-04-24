4718211320396fa4d267c66d35e1e45956945159
Philipp Kratzsch <philipp.kratzsch@electrolux.com>
2023-04-24 16:33:58 -0400

    Update

--- ArduinoReceive/ArduinoReceive.ino
+++ ArduinoReceive/ArduinoReceive.ino
@@ -123,47 +123,47 @@ void LeftElbow(int speed) {
 }
 
 void Driving(int speed) {
-switch (speed) {
-  case 1:
-    //Forward
-    roboclaw.ForwardM1(address, 64);
-    roboclaw.ForwardM2(address, 64);
-    break;
-  case 2:
-    //Backward
-    roboclaw.BackwardM1(address, 64);
-    roboclaw.BackwardM2(address, 64);
-    break;
-  case 3:
-    //Left
-    roboclaw.ForwardM1(address, 0);
-    roboclaw.ForwardM2(address, 64);
-    break;
-  case 4:
-    //Right
-    roboclaw.ForwardM1(address, 64);
-    roboclaw.ForwardM2(address, 0);
-    break;
-  case 5:
-    //Forward Right
-    roboclaw.ForwardM1(address, 64);
-    roboclaw.ForwardM2(address, 32);
-    break;
-  case 6:
-    //Forward Left
-    roboclaw.ForwardM1(address, 32);
-    roboclaw.ForwardM2(address, 64);
-    break;
-  case 7:
-    //Backward Right
-    roboclaw.BackwardM1(address, 32);
-    roboclaw.BackwardM2(address, 64);
-    break;
-  case 8:
-    //Backward Left
-    roboclaw.BackwardM1(address, 64);
-    roboclaw.BackwardM2(address, 32);
-    break;
+  switch (speed) {
+    case 1:
+      //Forward
+      roboclaw.ForwardM1(address, 64);
+      roboclaw.ForwardM2(address, 64);
+      break;
+    case 2:
+      //Backward
+      roboclaw.BackwardM1(address, 64);
+      roboclaw.BackwardM2(address, 64);
+      break;
+    case 3:
+      //Left
+      roboclaw.ForwardM1(address, 0);
+      roboclaw.ForwardM2(address, 64);
+      break;
+    case 4:
+      //Right
+      roboclaw.ForwardM1(address, 64);
+      roboclaw.ForwardM2(address, 0);
+      break;
+    case 5:
+      //Forward Right
+      roboclaw.ForwardM1(address, 64);
+      roboclaw.ForwardM2(address, 32);
+      break;
+    case 6:
+      //Forward Left
+      roboclaw.ForwardM1(address, 32);
+      roboclaw.ForwardM2(address, 64);
+      break;
+    case 7:
+      //Backward Right
+      roboclaw.BackwardM1(address, 32);
+      roboclaw.BackwardM2(address, 64);
+      break;
+    case 8:
+      //Backward Left
+      roboclaw.BackwardM1(address, 64);
+      roboclaw.BackwardM2(address, 32);
+      break;
   }
 }
 
